25c1f826acd60146b14febb84dd3803ac1e47d65
KirbytroNic0528 <133230754+KirbytroNic0528@users.noreply.github.com>
2024-10-09 09:42:11 +0800

    Update readcoverage.py

--- scripts/readcoverage.py
+++ scripts/readcoverage.py
@@ -2,6 +2,7 @@ from langchain_community.chat_models import ChatZhipuAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 
+
 def read_coverage(moonbit_code, index, api_key):
     read_prompt = ChatPromptTemplate.from_template(
         """你是一位技术高超的moonbit工程师，现在你需要读入moonbit语言的代码以及测试未覆盖代码行数的索引
@@ -12,24 +13,14 @@ def read_coverage(moonbit_code, index, api_key):
     )
 
     read_llm = ChatZhipuAI(
-        api_key=api_key,
-        model="glm-4-plus",
-        temperature=0.5,
-        max_tokens=2048
+        api_key=api_key, model="glm-4-0520", temperature=0.5
     )
 
-    read_retriever_chain = (
-        read_prompt
-        | read_llm
-        | StrOutputParser()
+    read_retriever_chain = read_prompt | read_llm | StrOutputParser()
+    response = read_retriever_chain.invoke(
+        {"moonbit_code": moonbit_code, "index": index}
     )
-    response = read_retriever_chain.invoke({
-        "moonbit_code": moonbit_code,
-         "index": index
-    })
     return response
-
-
 
 
 
