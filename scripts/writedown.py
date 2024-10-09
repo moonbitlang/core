3d4b2af2d00431c0b8d74efce44a8e064d8fdaa5
KirbytroNic0528 <133230754+KirbytroNic0528@users.noreply.github.com>
2024-10-09 09:43:49 +0800

    Update writedown.py

--- scripts/writedown.py
+++ scripts/writedown.py
@@ -1,7 +1,28 @@
+import subprocess
 import os
+def writedown_test_files(file_path, test_code):
+    with open(file_path, "a") as file:
+        file.write(test_code + "\n")
 
-def writedown_test_files(path, test_code):
-        with open(path, 'a') as file:
-            file.write(test_code + '\n')
+    print(f"test_code has been written to {file_path}")
 
-        print(f"test_code has been written to {path}")
+
+def writedown_and_test(response, file_path):
+    folder_path = os.path.dirname(file_path)
+    testcode_path = os.path.join(folder_path, "testcode.mbt")
+    with open(testcode_path, "w", encoding="utf-8") as file:
+        file.write(response)
+        try:
+            result = subprocess.run(
+                ["moon", "test", "-f", file_path], capture_output=True, text=True
+            )
+            if result.returncode != 0:
+                os.remove(testcode_path)
+                return 1
+            else:
+                os.remove(testcode_path)
+                writedown_test_files(file_path, response)
+                return 0
+        except Exception as e:
+            os.remove(testcode_path)
+            return 1
