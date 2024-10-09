7ce6b71fe58aa2c9d38e724cfff87c79d4a28614
KirbytroNic0528 <133230754+KirbytroNic0528@users.noreply.github.com>
2024-10-09 09:43:19 +0800

    Update testagent.py

--- scripts/testagent.py
+++ scripts/testagent.py
@@ -1,30 +1,47 @@
 import argparse
 import json
-from Agent.readcoverage import read_coverage
-from Agent.gettest import generate_test_code
-from Agent.writedown import writedown_test_files
+from scripts.readcoverage import read_coverage
+from scripts.gettest import generate_test_code
+from scripts.writedown import writedown_and_test
+
+
 def main():
-    with open('coveralls.json', 'r') as file:
+    with open("coveralls.json", "r") as file:
         data = json.load(file)
 
     parser = argparse.ArgumentParser(description="用于加载API密钥。")
-    parser.add_argument('--api_key', default="4a478b99108ee30c1ae4aaa0aefe6632.X8sj7A6gaBgWh9AE", type=str, help='API密钥')
+    parser.add_argument(
+        "--api_key",
+        type=str,
+        help="API密钥",
+    )
     args = parser.parse_args()
     zhipuai_api_key = args.api_key
 
-    for source_file in data['source_files']:
-        with open(source_file['name'], "r") as codefile:
-            index = [index for index, value in enumerate(source_file['coverage']) if value == 0]             
-            if index!=[]:
-              print(index)
-              moonbit_code = codefile.read()
-              response = read_coverage(moonbit_code,index, zhipuai_api_key)
-              print("未覆盖的函数声明为"+response)
-              response = generate_test_code(response,source_file['name'],zhipuai_api_key)
-              test_code = response.replace("```moonbit\n", "").rstrip("```")  
-              writedown_test_files(source_file['name'], test_code)
-              print("测试用例为"+test_code)
-              
+    for source_file in data["source_files"]:
+        with open(source_file["name"], "r") as codefile:
+            indexs = [
+                indexs
+                for indexs, value in enumerate(source_file["coverage"])
+                if value == 0
+            ]
+            if indexs:
+                print(indexs)
+                moonbit_code = codefile.read()
+                for index in indexs:
+                    for attempt in range(3):
+                        response = read_coverage(moonbit_code, index, zhipuai_api_key)
+                        print("未覆盖的函数声明为" + response)
+                        response = generate_test_code(
+                            response, source_file["name"], zhipuai_api_key
+                        )
+                        test_code = response.replace("```moonbit\n", "").rstrip("```")
+                        print("测试用例为" + test_code)
+                        result = writedown_and_test(response, source_file["name"])
+
+                        if result == 0:
+                            break
+
 
 if __name__ == "__main__":
     main()
