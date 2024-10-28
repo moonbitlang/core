4346c597da42048cd2b2415214df86edb0f354d3
KirbytroNic0528 <133230754+KirbytroNic0528@users.noreply.github.com>
2024-10-29 01:25:36 +0800

    Update writedown.py

--- scripts/writedown.py
+++ scripts/writedown.py
@@ -1,4 +1,4 @@
-from gettest import rethink_test_code, generate_test_code
+from gettest import rethink_test_code
 import subprocess
 import os
 
@@ -14,8 +14,8 @@ def test_test_code(moonbit_code, test_code, file_path, api_key):
     folder_path = os.path.dirname(file_path)
     package_name = os.path.basename(os.path.dirname(file_path))
     testcode_path = os.path.join(folder_path, "testcode.mbt")
-    with open(testcode_path, "w", encoding="utf-8") as file:
-        file.write(test_code)
+    with open(testcode_path, "w", encoding="utf-8") as test_file:
+        test_file.write(test_code)
         test_result = subprocess.run(
             ["moon", "test", "-p", package_name, "-f", file_path],
             capture_output=True,
@@ -25,12 +25,13 @@ def test_test_code(moonbit_code, test_code, file_path, api_key):
         max_attempts = 3
         while test_result.returncode and attempts < max_attempts:
             attempts += 1
-            file.truncate(0)
-            new_test_code = generate_test_code(
-                        moonbit_code, file_path, api_key
+            test_file.truncate(0)
+            new_test_code = rethink_test_code(
+                        moonbit_code, test_code, file_path, api_key
                     )
-            file.write(new_test_code)
-            file.flush()
+            test_file.write(new_test_code)
+            test_file.flush()
+            print(new_test_code)
             test_result = subprocess.run(
                 ["moon", "test", "-p", package_name, "-f", file_path],
                 capture_output=True,
@@ -39,9 +40,11 @@ def test_test_code(moonbit_code, test_code, file_path, api_key):
         if attempts == max_attempts:
             print("get test_code fail")
             os.remove(testcode_path)
+            test_file.close()
             return
 
         os.remove(testcode_path)
         writedown_test_files(file_path, test_code)
         print("get test_code success")
+        test_file.close()
         return
